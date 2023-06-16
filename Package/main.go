5eb348eed44bd478a6fe1ef0699f623d1023e8a8
karthickmarisamy <srikarthick91@gmail.com>
2023-06-16 17:02:31 +0530

    Interface, encapsolution and Concurrency

--- Package/main.go
+++ Package/main.go
@@ -3,6 +3,7 @@ import (
 	"fmt"
 	stuff "sample/project/mypackage"
 	"reflect"
+	"log"
 )
 
 func main(){
@@ -12,5 +13,22 @@ func main(){
 	fmt.Println(strArry)
 	fmt.Println(reflect.TypeOf(strArry))
 
+	date := stuff.Date{}
 
+	var err = date.SetDay(1)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	err = date.SetMonth(1)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	err = date.SetYear(1991)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	fmt.Printf("The Current data is %d-%d-%d\n", date.Date(), date.Month(), date.Year())
 }
--- Package/mypackage/mypackage.go
+++ Package/mypackage/mypackage.go
@@ -2,6 +2,8 @@ package stuff
 
 import (
 	"strconv"
+	"errors"
+	"time"
 )
 
 var Name string = "Karthick"
@@ -13,3 +15,47 @@ func IntArrytoStrArry(iArry []int)[]string{
 	}
 	return sArry;
 }
+
+
+type Date struct{
+	day int
+	month int
+	year int
+}
+
+func (d *Date) SetDay(day int)error{
+	if(day < 1 && day > 31){
+		errors.New("Invalid Day")
+	}
+	d.day = day
+	return nil
+}
+
+func (d *Date) SetMonth(month int)error{
+	if(month < 1 && month > 12){
+		errors.New("Invalid Month")
+	}
+	d.month = month
+	return nil
+}
+
+func (d *Date) SetYear(year int)error{
+	if(year < 1875 && year > time.Now().Year()){
+		errors.New("Invalid Year")
+	}
+	d.year = year
+	return nil
+}
+
+func (d *Date) Date()int{
+	return d.day
+}
+
+func (d *Date) Month()int{
+	return d.month
+}
+
+func (d *Date) Year()int{
+	return d.year
+}
+
